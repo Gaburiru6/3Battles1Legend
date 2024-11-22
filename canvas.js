dc4ccf2f42b30e9a280971145c5678b9c5bfa1b3
unknown <luiscff2001@hotmail.com>
2024-11-22 09:47:20 -0300

    .

--- canvas.js
+++ canvas.js
@@ -1,6 +1,6 @@
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d") ;
 
-canvas.width = 1240; //largura canva (3720 mapa completo)
-canvas.height = 720; //altura canva  (2160 mapa completo)
+canvas.width = 1240; //largura canva (1240)(3720 mapa completo)
+canvas.height = 720; //altura canva  (720)(2160 mapa completo)
 canvas.style.backgroundColor = "darkgray";
