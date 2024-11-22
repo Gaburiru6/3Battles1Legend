eaf937cc08d8dc4f74912b6d2d3c5ec436752efa
LisinhaDeGranaaaa <gachalisa2020@gmail.com>
2024-11-22 15:58:24 -0300

    menu funciona, porco sumiu :P

--- npc/pig.js
+++ npc/pig.js
@@ -12,16 +12,10 @@ function drawPig() {
 }
 
 function piganimate(){
-<<<<<<< HEAD
-    //console.log("desenhando pig");
-    ctx.clearRect((canvas.width/2) -50, (canvas.height/2)-205,largura*3,altura*2);
-    ctx.drawImage(pig, largura * frame, 0, largura, altura, (canvas.width/2) -50, (canvas.height/2) -220, largura*3, altura*3);
-=======
     ctxsprite.clearRect((canvas.width/2) +30, (canvas.height/2)-190,largura*2,altura*2);
     ctxsprite.drawImage(pig, largura * framepig, 0, largura, altura, (canvas.width/2) +30, (canvas.height/2) -190, largura*2, altura*2);
     ctxsprite.clearRect((canvas.width/2) -80, (canvas.height/2)-205,largura*2,altura*2);
     ctxsprite.drawImage(pig, largura * framepig, 0, largura, altura, (canvas.width/2) -80, (canvas.height/2) -205, largura*2.5, altura*2.5);
->>>>>>> 06768da6457cdbe53615cc26b686f5354decc49c
     framepig++;
     if(framepig == 2){
         framepig = 0;
