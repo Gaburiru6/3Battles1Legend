5581888702b3b4565691358a712d97b5f5b11a36
LuiscffBRA <luiscff2001@hotmail.com>
2024-11-24 05:27:09 -0300

    Logica da colisao

--- class.js
+++ class.js
@@ -11,29 +11,38 @@ class Borda {
     }
 
     draw(){
-        ctx.fillStyle = 'rgba(255,0,0,0.6)'
+        ctx.fillStyle = 'rgba(255,0,0,0.3)'
         ctx.fillRect(this.position.x,this.position.y,this.width,this.height)
     }
 }
 
 class Sprite{
-    constructor({position, velocity, map}){
+    constructor({position, velocity, map, frames = {max: 1}, lines = {line: 1}}){
         this.position = position
         this.map = map
+        this.frames = frames
+        this.lines = lines
+
+        this.map.onload = () => {
+            this.width = this.map.width/this.frames.max 
+            this.height = this.map.height/this.lines.line
+        }
+        
     }
 
     draw(){
-        ctx.drawImage(this.map,this.position.x,this.position.y)
-        ctx.drawImage(playerImage,
-            0,
-            0,
-            playerImage.width / 6,
-            playerImage.height / 10,
-            canvas.width / 2 - (playerImage.width / 6) / 2,
-            canvas.height / 2 - (playerImage.height / 10) / 2,
-            playerImage.width / 6,
-            playerImage.height / 10
+        ctx.drawImage(
+            this.map,                                           // A imagem a ser desenhada
+            0,                                                  // Posição X inicial da fatia (crop) da imagem original
+            0,                                                  // Posição Y inicial da fatia (crop) da imagem original
+            this.map.width / this.frames.max,                   // Largura da fatia (crop) da imagem original
+            this.map.height / this.lines.line,                              // Largura da fatia (crop) da imagem original
+            this.position.x,          // Posição X onde a fatia será desenhada no canvas 
+            this.position.y,       // Posição Y onde a fatia será desenhada no canvas
+            this.map.width / this.frames.max,                                 // Largura da fatia no canvas (redimensionada)
+            this.map.height / this.lines.line                             // Altura da fatia no canvas (redimensionada)
         );
     }
+    
 }
 
