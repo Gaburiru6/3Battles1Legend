bf0a37cb0670828f13a42ca60bef839a6153977f
LuiscffBRA <luiscff2001@hotmail.com>
2024-11-24 15:38:53 -0300

    Adicionada troca de imagens nas respectivaas direcoes, e otimizacao no keyup

--- personagem/movimentacao.js
+++ personagem/movimentacao.js
@@ -1,38 +1,40 @@
-let lastKey = ''
 window.addEventListener('keydown', (event) => {
-    switch(event.key){
+    switch (event.key) {
         case 'w':
-            keys.w.pressed = true
-            lastKey = 'w'
-            break
+            keys.w.pressed = true;
+            lastKey = 'w';
+            player.currentDirection = 160; // Andar para cima
+            player.movendo = true;
+            break;
         case 'a':
-            keys.a.pressed = true
-            lastKey = 'a'
-            break
+            keys.a.pressed = true;
+            lastKey = 'a';
+            player.currentDirection = 127; // Andar para a esquerda
+            player.movendo = true;
+            break;
         case 's':
-            keys.s.pressed = true
-            lastKey = 's'
-            break
+            keys.s.pressed = true;
+            lastKey = 's';
+            player.currentDirection = 96; // Andar para baixo
+            player.movendo = true;
+            break;
         case 'd':
-            keys.d.pressed = true
-            lastKey = 'd'
-            break
+            keys.d.pressed = true;
+            lastKey = 'd';
+            player.currentDirection = 128; // Andar para a direita
+            player.movendo = true;
+            break;
     }
-})
+});
 
 window.addEventListener('keyup', (event) => {
-    switch(event.key){
+    switch (event.key) {
         case 'w':
-            keys.w.pressed = false
-            break
         case 'a':
-            keys.a.pressed = false
-            break
         case 's':
-            keys.s.pressed = false
-            break
         case 'd':
-            keys.d.pressed = false
-            break
+            keys[event.key].pressed = false;
+            player.movendo = false; // Parar movimento ao soltar a tecla
+            break;
     }
-})
+});
