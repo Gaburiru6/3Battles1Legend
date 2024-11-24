76284085e27ab7595187fce38ea1cb445f287a04
LuiscffBRA <luiscff2001@hotmail.com>
2024-11-24 02:23:59 -0300

    .

--- mapa/mapa.js
+++ mapa/mapa.js
@@ -1,23 +1,4 @@
-const coliMap = [];
-
-for(let i=0; i < colisao.length; i+= 70){
-    coliMap.push(colisao.slice(i,70+i)); // coloca o map de colisao como array a cada 70 numeros
-}
-
 const bordas = [];
-
-coliMap.forEach((row,i) =>{//cada linha sendo id I
-    row.forEach((symbol,j) =>{// cada numero na linha, ou seja as colunas de id J
-        if(symbol === 1)//se o numero for de colisao
-        bordas.push(
-        new Borda({
-            position:{
-                x: j*Borda.width + offset.x,//coloca as colisoes no local certo e um do lado do outro
-                y: i*Borda.height + offset.y,// assim cada uma tem seu x e y
-            } 
-        }));
-    })
-})
 
 const map = new Image();//cria a constante da imagem
 map.src = 'mapa.png'//pega a imagem
