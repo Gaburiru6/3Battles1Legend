1c7df6851ae1b5e4c9f19072baf8f81072c43b3e
unknown <luiscff2001@hotmail.com>
2024-11-25 15:02:21 -0300

    Telas finais atualizadas

---
+++ Finais/vitoria.js
@@ -0,0 +1,31 @@
+// const canvasM = document.getElementById('gameMenuCanvas');
+// const ctxM = canvasM.getContext("2d")
+// // Ajustar o canvas para ocupar a tela inteira
+// canvasM.width = 1240;
+// canvasM.height = 720;
+
+// // Carregar a imagem de fundo
+// const backgroundImage = new Image();
+// backgroundImage.src = './Finais/finalVitoria.png';
+
+// if(o jogador ganhar){
+//     // Função para desenhar o título do jogo com contorno
+//         ctxM.font = "80px 'MedievalSharp', sans-serif"; // Fonte do título
+//         ctxM.fillStyle = '#f4a460'; // Cor do texto
+//         ctxM.textAlign = 'center';
+//         ctxM.textBaseline = 'middle';
+
+//         // Desenha o contorno
+//         ctxM.lineWidth = 5; // Largura do contorno
+//         ctxM.strokeStyle = 'black'; // Cor do contorno
+//         ctxM.strokeText("Parabéns, você salvou a vila", canvasM.width / 2, 150); // Desenha o contorno
+
+//         // Desenha o texto principal
+//         ctxM.fillText("Parabéns, você salvou a vila", canvasM.width / 2, 150); // Desenha o texto em si
+//     }
+
+//     // Função para desenhar o fundo da imagem
+//     function drawBackground() {
+//         ctxM.drawImage(backgroundImage, 0, 0, canvasM.width, canvasM.height);
+//     }
+// }
