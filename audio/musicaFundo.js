a3996076c9c65a589fb9f49d3db52e7946dacb3f
LuiscffBRA <luiscff2001@hotmail.com>
2024-11-29 00:31:52 -0300

    Adicionado suporte para mutar som do jogo.

--- audio/musicaFundo.js
+++ audio/musicaFundo.js
@@ -1,13 +1,31 @@
 const musicaFundo = new Audio('./audio/medievalMusic.mp3');
 musicaFundo.loop = true;
-musicaFundo.volume = 1;
+musicaFundo.volume = 1;  // Som no máximo
 
+// Função para tocar música de fundo
 function tocarMusicaFundo() {
     musicaFundo.play().catch((error) => {
         console.error("Erro ao tentar tocar a música:", error);
     });
 }
 
+// Função para mutar/desmutar a música
+function toggleMute() {
+    if (musicaFundo.muted) {
+        musicaFundo.muted = false;
+        document.getElementById("muteButton").textContent = "Mutar";  // Altera para "Mutar"
+    } else {
+        musicaFundo.muted = true;
+        document.getElementById("muteButton").textContent = "Desmutar";  // Altera para "Desmutar"
+    }
+}
+
+// Evento de clique no body para começar a música
 document.body.addEventListener('click', () => {
     tocarMusicaFundo();
+});
+
+// Evento de clique no botão para alternar o estado de áudio
+document.getElementById("muteButton").addEventListener('click', () => {
+    toggleMute();
 });
