1a1448b7e921ac9dff7a594852a31e1cf684c8a8
unknown <luiscff2001@hotmail.com>
2024-12-02 08:54:29 -0300

    Atualizado

--- MenuGame/gameMenu.js
+++ MenuGame/gameMenu.js
@@ -1,5 +1,5 @@
 const canvasM = document.getElementById('gameMenuCanvas');
-const ctxM = canvasM.getContext("2d");
+const ctxM = canvasM.getContext("2d")
 // Ajustar o canvas para ocupar a tela inteira
 canvasM.width = 1240;
 canvasM.height = 720;
@@ -17,26 +17,9 @@ const buttons = [
 const backgroundImage = new Image();
 backgroundImage.src = './MenuGame/BackGroundMenu.png';
 
-// Música de fundo
-const musicaFundo = new Audio('./audio/medievalMusic.mp3');
-musicaFundo.loop = true;
-
-// Verificar estado da música (mutada ou não) no localStorage
-let musicaMutada = localStorage.getItem('musicaMutada') === 'true'; // Se estiver armazenado como 'true', a música começa mutada
-musicaFundo.volume = musicaMutada ? 0 : 1; // Se for mutado, a música inicia no volume 0
-
-// Função para tocar a música
-function tocarMusicaFundo() {
-    if (musicaFundo.paused) {
-        musicaFundo.play().catch((error) => {
-            console.error("Erro ao tentar tocar a música:", error);
-        });
-    }
-}
-
 // Função para desenhar o título do jogo com contorno
 function drawTitle() {
-    ctxM.font = "90px 'MedievalSharp', sans-serif"; // Fonte do título
+    ctxM.font = "80px 'MedievalSharp', sans-serif"; // Fonte do título
     ctxM.fillStyle = '#f4a460'; // Cor do texto
     ctxM.textAlign = 'center';
     ctxM.textBaseline = 'middle';
@@ -53,110 +36,17 @@ function drawTitle() {
 // Função para desenhar os botões
 function drawButtons() {
     buttons.forEach(button => {
-        const increasedWidth = button.width * 1.2;
-        const increasedHeight = button.height * 1.2;
-
-        // Tamanho do botão quando o mouse está sobre ele
-        const buttonWidthWithHover = button.isHovered ? increasedWidth * 1.1 : increasedWidth;
-        const buttonHeightWithHover = button.isHovered ? increasedHeight * 1.1 : increasedHeight;
-
-        const buttonXWithHover = (canvasM.width - buttonWidthWithHover) / 2;
-        const buttonYWithHover = button.y - (buttonHeightWithHover - increasedHeight) / 2;
-
-        const buttonColor = '#f4a460';
-
-        ctxM.fillStyle = buttonColor;
-        ctxM.strokeStyle = 'black';
-        ctxM.lineWidth = 3;
-
-        ctxM.beginPath();
-        ctxM.moveTo(buttonXWithHover + 10, buttonYWithHover);
-        ctxM.lineTo(buttonXWithHover + buttonWidthWithHover - 10, buttonYWithHover);
-        ctxM.lineTo(buttonXWithHover + buttonWidthWithHover, buttonYWithHover + 10);
-        ctxM.lineTo(buttonXWithHover + buttonWidthWithHover, buttonYWithHover + buttonHeightWithHover - 10);
-        ctxM.lineTo(buttonXWithHover + buttonWidthWithHover - 10, buttonYWithHover + buttonHeightWithHover);
-        ctxM.lineTo(buttonXWithHover + 10, buttonYWithHover + buttonHeightWithHover);
-        ctxM.lineTo(buttonXWithHover, buttonYWithHover + buttonHeightWithHover - 10);
-        ctxM.lineTo(buttonXWithHover, buttonYWithHover + 10);
-        ctxM.closePath();
-        ctxM.fill();
-        ctxM.stroke();
-
-        const fontSize = button.isHovered ? 60 : 50; // Tamanho maior quando o mouse está sobre o botão
+        if (button.isHovered) {
+            ctxM.fillStyle = '#f39c12'; // Cor quando o botão é hover
+        } else {
+            ctxM.fillStyle = 'black'; // Cor verde floresta padrão
+        }
+        ctxM.fillRect(button.x, button.y, button.width, button.height);
 
         ctxM.fillStyle = 'white';
-        ctxM.font = `${fontSize}px 'MedievalSharp', sans-serif`;
-        ctxM.textAlign = 'center';
-        ctxM.textBaseline = 'middle';
-
-        // Desenha o contorno do texto
-        ctxM.lineWidth = 5;
-        ctxM.strokeStyle = 'black';
-        ctxM.strokeText(button.text, buttonXWithHover + buttonWidthWithHover / 2, buttonYWithHover + buttonHeightWithHover / 2);
-
-        // Desenha o texto principal
-        ctxM.fillText(button.text, buttonXWithHover + buttonWidthWithHover / 2, buttonYWithHover + buttonHeightWithHover / 2);
+        ctxM.font = "50px 'MedievalSharp', sans-serif"; // Usando a mesma fonte do título
+        ctxM.fillText(button.text, button.x + button.width / 2, button.y + button.height / 2);
     });
-}
-
-// Função para desenhar o botão de mutar/desmutar com animação de aumento
-function drawMuteButton() {
-    const buttonWidth = 150;
-    const buttonHeight = 70;
-    const buttonX = (canvasM.width - buttonWidth) / 2;
-    const buttonY = canvasM.height - 150;
-
-    // Redefine o tamanho do botão
-    const increasedWidth = buttonWidth * 1.05;  // Tamanho do botão de 1.05
-    const increasedHeight = buttonHeight * 1.05;  // Tamanho do botão de 1.05
-
-    // Tamanho do botão de mutar/desmutar com hover
-    const buttonWidthWithHover = musicaMutada ? increasedWidth * 1.1 : increasedWidth;
-    const buttonHeightWithHover = musicaMutada ? increasedHeight * 1.1 : increasedHeight;
-
-    const buttonXWithHover = (canvasM.width - buttonWidthWithHover) / 2;
-    const buttonYWithHover = buttonY - (buttonHeightWithHover - increasedHeight) / 2;
-
-    const buttonColor = '#f4a460';
-
-    ctxM.fillStyle = buttonColor;
-    ctxM.strokeStyle = 'black';
-    ctxM.lineWidth = 3;
-
-    ctxM.beginPath();
-    ctxM.moveTo(buttonXWithHover + 10, buttonYWithHover);
-    ctxM.lineTo(buttonXWithHover + buttonWidthWithHover - 10, buttonYWithHover);
-    ctxM.lineTo(buttonXWithHover + buttonWidthWithHover, buttonYWithHover + 10);
-    ctxM.lineTo(buttonXWithHover + buttonWidthWithHover, buttonYWithHover + buttonHeightWithHover - 10);
-    ctxM.lineTo(buttonXWithHover + buttonWidthWithHover - 10, buttonYWithHover + buttonHeightWithHover);
-    ctxM.lineTo(buttonXWithHover + 10, buttonYWithHover + buttonHeightWithHover);
-    ctxM.lineTo(buttonXWithHover, buttonYWithHover + buttonHeightWithHover - 10);
-    ctxM.lineTo(buttonXWithHover, buttonYWithHover + 10);
-    ctxM.closePath();
-    ctxM.fill();
-    ctxM.stroke();
-
-    const fontSize = 36;  // Alteração no tamanho da fonte para 36px
-
-    ctxM.fillStyle = 'white';
-    ctxM.font = `${fontSize}px 'MedievalSharp', sans-serif`;
-    ctxM.textAlign = 'center';
-    ctxM.textBaseline = 'middle';
-
-    // Desenha o contorno do texto
-    ctxM.lineWidth = 5;
-    ctxM.strokeStyle = 'black';
-    ctxM.strokeText(musicaMutada ? "Desmutar" : "Mutar", buttonXWithHover + buttonWidthWithHover / 2, buttonYWithHover + buttonHeightWithHover / 2);
-
-    // Desenha o texto principal
-    ctxM.fillText(musicaMutada ? "Desmutar" : "Mutar", buttonXWithHover + buttonWidthWithHover / 2, buttonYWithHover + buttonHeightWithHover / 2);
-}
-
-// Função para alternar entre mutar/desmutar
-function toggleMute() {
-    musicaMutada = !musicaMutada;
-    musicaFundo.volume = musicaMutada ? 0 : 1; // Ajusta o volume
-    localStorage.setItem('musicaMutada', musicaMutada); // Salva o estado no localStorage
 }
 
 // Função para verificar se o mouse está sobre um botão
@@ -178,7 +68,6 @@ function updateMenu() {
     drawBackground();
     drawTitle();
     drawButtons();
-    drawMuteButton(); // Desenha o botão de mutar/desmutar
 }
 
 // Função que lida com os cliques do mouse
@@ -191,20 +80,15 @@ canvasM.addEventListener('click', (e) => {
             if (button.text === "Jogar") {
                 canvasM.style.display = 'none'; 
                 cutsc.style.display = 'block';
-                updateCutsc();
+
+                updateCutsc()
+                //alert("Iniciar o jogo..."); //-Teste para ver se o botão está funcionando
             } else if (button.text === "Sair") {
+                // alert("Saindo..."); -Teste para ver se o botão está funcionando
                 window.close();
             }
         }
     });
-
-    // Verifica se o clique foi no botão de mutar/desmutar
-    const muteButtonX = (canvasM.width - 150) / 2;
-    const muteButtonY = canvasM.height - 150;
-    if (mouseX > muteButtonX && mouseX < muteButtonX + 150 && mouseY > muteButtonY && mouseY < muteButtonY + 70) {
-        toggleMute(); // Alterna entre mutar e desmutar
-        updateMenu(); // Atualiza o menu
-    }
 });
 
 // Função que lida com o movimento do mouse
@@ -217,6 +101,12 @@ canvasM.addEventListener('mousemove', (e) => {
 
 // Inicializa o menu
 backgroundImage.onload = () => {
-    tocarMusicaFundo(); // Começa a música ao carregar a página
-    updateMenu(); // Atualiza o menu
+    updateMenu(); // Chama a função que desenha o menu depois da imagem carregar
 };
+
+// Ajusta o tamanho do canvas ao redimensionar a janela
+// window.addEventListener('resize', () => {
+//     canvasM.width = window.innerWidth;
+//     canvasM.height = window.innerHeight;
+//     updateMenu(); // Atualiza a tela quando a janela for redimensionada
+// });
